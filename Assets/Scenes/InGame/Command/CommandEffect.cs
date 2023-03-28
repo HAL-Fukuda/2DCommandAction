0c16b7a70f9136a948cf6c27101260b633f0893c
kirihito <fancy.design0818@gmail.com>
2023-03-29 03:43:53 +0900

    コマンドの破壊とエフェクトの再生を実装。 新しく追加・作成したもの 　・CommandEffect.cs　・CommandSEオブジェクト(Prefab化済み）　・EffectExplosionオブジェクト(Prefab化済み）・ExplosionMat 　・Explosion01(画像) 　・ExplosionSE(音声ファイル) 現在は、Playerタグが付いている物が接触すると破壊とエフェクト再生が起こるようになっている。 エフェクトは現在爆発になっているが、今後ゲーム性に沿ってエフェクトを差し替えることもできるようにしてある。

---
+++ Assets/Scenes/InGame/Command/CommandEffect.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandEffect : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    [SerializeField] private ParticleSystem EffectPrefab;  //EffectのPrefabを設定
+    [SerializeField] private GameObject     SoundPrefab;   //SoundのPrefabを設定
+
+    private ParticleSystem _effectInstance;
+    private GameObject _soundInstance;
+   
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();  //衝突時にオブジェクトを消す際に使用
+    }
+    
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("HIT");
+
+            EffectPlay();
+
+            //コマンドを削除
+            Destroy(gameObject);
+        }
+    }
+
+    void EffectPlay()
+    {
+        //ParticleSystemのインスタンスを生成
+        _effectInstance = Instantiate(EffectPrefab);
+        //Effectの発生位置をコマンドの位置にする
+        _effectInstance.transform.position = this.transform.position;
+        //Effectを再生
+        _effectInstance.Play();
+
+        //SEを再生
+        SoundPlay();
+    }
+
+    void SoundPlay()
+    {
+        //Soundのインスタンスを生成
+        _soundInstance = Instantiate(SoundPrefab);
+        //Soundの発生位置をコマンドの位置にする
+        _soundInstance.transform.position = this.transform.position;
+        //Soundは発生と同時に鳴るようにしている
+    }
+    //private void Awake()
+    //{
+    //    _effectInstance = Instantiate(EffectPrefab, transform);
+    //    Debug.Log(_effectInstance);
+    //}
+
+    //void OnCollisionEnter2D(Collision2D col)
+    //{
+    //    if (col.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("HIT");
+
+    //        EffectPlay();  //Effect再生
+    //        if (Input.GetKey(KeyCode.Return))
+    //        {
+    //            Debug.Log("Return");
+    //        }
+    //    }
+    //}
+
+    //void EffectPlay()
+    //{
+    //    if (_effectInstance)
+    //    {
+    //        //ぶつかった位置にEffectのprefabを配置する
+    //        Instantiate(EffectPrefab, this.transform.position, Quaternion.identity);
+    //        _effectInstance.Play();
+    //        Debug.Log("EffectPlay");
+    //        //Destroy(gameObject);  //オブジェクト破壊
+    //    }
+    //}
+}
