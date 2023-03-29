a3949eb44a82cbf4d7988d8df607b13cadf30aa7
TiG401 <tig13.0401@gmail.com>
2023-03-29 18:52:00 +0900

    敵の攻撃Meteorのスクリプトの修正

---
+++ Assets/Scenes/InGame/Enemy/EnemyScript/MeteorEnemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public partial class EnemyAttack : MonoBehaviour //クラス名統一
+{
+    public AttackSettings meteorAttackSettings;
+
+    public void MeteorAttack()
+    {
+
+        meteorAttackSettings.timer += Time.deltaTime;
+
+        if (meteorAttackSettings.timer >= meteorAttackSettings.spawnInterval)
+        {
+            meteorAttackSettings.timer = 0f;
+
+            // 画面外にオブジェクトを生成する
+            Vector3 spawnPos = new Vector3(Random.Range(-10f, 10f), 7f, 0f);//Random.Range(-10f, 10f), 7f, 0f
+            GameObject obj = Instantiate(meteorAttackSettings.prefab, spawnPos, Quaternion.identity);
+
+            obj.transform.DOMoveY(
+                -30f, //移動量
+                meteorAttackSettings.duration // 演出時間
+            ).OnComplete(() =>
+            {
+                Destroy(obj);
+            });
+        }
+    }
+}
