47a341e37e69270d492f683c006c9b1a752688a0
HAL_Fukuda <91521810+HAL-Fukuda@users.noreply.github.com>
2023-03-30 21:55:13 +0900

    LifeManagerにGetDamage()とGetHeal()を追加。 引数の値の数だけ回復やダメージを受ける。

--- Assets/Scenes/InGame/UI/Life/LifeManager.cs
+++ Assets/Scenes/InGame/UI/Life/LifeManager.cs
@@ -5,20 +5,55 @@ using UnityEngine;
 public class LifeManager : MonoBehaviour
 {
     public GameObject[] lifeArray = new GameObject[5];
-    private int lifePoint = 5;
+    public int lifePoint = 5;
+    public int maxlifePoint = 5;
+
+    // 被ダメージ
+    // 引数の値の文だけHPが減る
+    void GetDamage(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            if (lifePoint > 0)
+            {
+                lifeArray[lifePoint - 1].SetActive(false);
+                lifePoint--;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    // 回復
+    // 引数の値の数だけHPが増える
+    void GetHeel(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            if (lifePoint < maxlifePoint)
+            {
+                lifePoint++;
+                lifeArray[lifePoint - 1].SetActive(true);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && lifePoint < 5)
+        if (Input.GetMouseButtonDown(0))
         {
-            lifePoint++;
-            lifeArray[lifePoint - 1].SetActive(true);
+            GetDamage(2);
         }
 
-        else if (Input.GetMouseButtonDown(1) && lifePoint > 0)
+        else if (Input.GetMouseButtonDown(1))
         {
-            lifeArray[lifePoint - 1].SetActive(false);
-            lifePoint--;
+            GetHeel(3);
         }
     }
 }
