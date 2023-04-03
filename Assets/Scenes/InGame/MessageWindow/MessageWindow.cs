7707023356e94471849aef434535276aec63d5ca
rincha- <rincha1210@gmail.com>
2023-04-03 21:00:39 +0900

    messagewinodwスクリプトを修正 別スクリプトから呼び出せるようにした　はず

--- Assets/Scenes/InGame/MessageWindow/MessageWindow.cs
+++ Assets/Scenes/InGame/MessageWindow/MessageWindow.cs
@@ -6,24 +6,40 @@ using UnityEngine.UI;
 public class MessageWindow : MonoBehaviour
 {
     public Text debugText;
+    public string message = "";
 
     // Start is called before the first frame update
     void Start()
     {
         // テキストコンポーネントを取得する
         debugText = GetComponent<Text>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // GameMgrのデバッグメッセージを取得する
-        string message = GameMgr.Instance.GetDebugMessage();
+        message = GetDebugMessage();
         if (!string.IsNullOrEmpty(message))
         {
             // デバッグメッセージを改行してテキストに追加する
             debugText.text += message + "\n";
         }
+    }
+
+    // メッセージを取得する
+    public string GetDebugMessage()
+    {
+        message = "";
+
+        //下の行で別スクリプトのmessageを呼び出す
+        //debugText.text = onoff.message;
+
+        // MessageWindowのテキストをクリアする
+        //messageWindow.ClearText(); 
+
+        return message;
     }
 
     public void ClearText()
