4c0520379474873aa7da9230e995de3f7f002105
kalta <kalta@MSI>
2023-04-09 23:20:49 +0900

    commandが滑らなくなった、サテライトビームに当たり判定追加

---
+++ Assets/Scenes/InGame/Enemy/SatelliteBeam/SatelliteBeamCollider.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatelliteBeamCollider : MonoBehaviour
+{
+    private LifeManager lifeManager;
+
+    private void Start()
+    {
+        // LifeManagerスクリプトがアタッチされているオブジェクトを取得する
+        GameObject lifeManagerObject = GameObject.Find("Life");
+
+        // LifeManagerコンポーネントを取得する
+        lifeManager = lifeManagerObject.GetComponent<LifeManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lifeManager.GetDamage(1);
+            //Destroy(gameObject);
+            Debug.Log("入った");
+        }
+    }
+
+}
