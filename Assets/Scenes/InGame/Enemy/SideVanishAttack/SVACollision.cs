3b47d921bb2382b37a8b6400549def298c130843
kalta <kalta@MSI>
2023-04-12 16:02:01 +0900

    飛ぶ斬撃の実装　＊当たり判定の関係上platformに変更を加えました

--- Assets/Scenes/InGame/Enemy/SideVanishAttack/SVACollision.cs
+++ Assets/Scenes/InGame/Enemy/SideVanishAttack/SVACollision.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class SVACollision : MonoBehaviour
 {
-    
+    private LifeManager lifeManager;
+
+    private void Start()
+    {
+        // LifeManagerスクリプトがアタッチされているオブジェクトを取得する
+        GameObject lifeManagerObject = GameObject.Find("Life");
+
+        // LifeManagerコンポーネントを取得する
+        lifeManager = lifeManagerObject.GetComponent<LifeManager>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Debug.Log("syoutoru");
-        Destroy(this.gameObject);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lifeManager.GetDamage(1);
+            //Debug.Log("1damage");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
     }
 }
