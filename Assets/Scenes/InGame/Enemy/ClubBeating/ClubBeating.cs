29f55d2abd7ec0a4574d5c0e7af9dd1863f2e019
kalta <kalta@MSI>
2023-04-18 20:09:58 +0900

    狼の噛みつきの暫定版実装

--- Assets/Scenes/InGame/Enemy/ClubBeating/ClubBeating.cs
+++ Assets/Scenes/InGame/Enemy/ClubBeating/ClubBeating.cs
@@ -56,7 +56,7 @@ public partial class EnemyAttack : MonoBehaviour
     }
 
 
-    // Update is called once per frame
+    //Update is called once per frame
     //void Update()
     //{
     //    if (Input.GetKeyUp(KeyCode.B))
---
+++ Assets/Scenes/InGame/Enemy/WolfBiting/WBCollision.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WBCollision : MonoBehaviour
+{
+    private LifeManager lifeManager;
+
+    private BoxCollider2D boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // LifeManagerスクリプトがアタッチされているオブジェクトを取得する
+        GameObject lifeManagerObject = GameObject.Find("Life");
+
+        // LifeManagerコンポーネントを取得する
+        lifeManager = lifeManagerObject.GetComponent<LifeManager>();
+
+        // Box Collider 2D コンポーネントの参照を取得する
+        boxCollider = this.GetComponent<BoxCollider2D>();
+
+    }
+
+    public void ToggleCollider(bool enabled)
+    {
+        // Box Collider 2D コンポーネントの enabled プロパティを設定する
+        boxCollider.enabled = enabled;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lifeManager.GetDamage(1);
+            //Debug.Log("1damage");
+            //audioSource.PlayOneShot(BeatingSound);  // サウンドを鳴らす
+        }
+        else
+        {
+            //audioSource.PlayOneShot(BeatingSound);
+        }
+
+    }
+}
