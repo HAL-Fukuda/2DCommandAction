5864c0d38b1cf7b58e61a6b316b7edbf5f88d6c2
kalta <kalta@MSI>
2023-04-20 22:17:57 +0900

    ステージセレクトの実装、画像はステージ選択以外仮置き

---
+++ Assets/Scenes/StageSelect/BackWorldSelect.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class BackWorldSelect : MonoBehaviour
+{
+    public GameObject WorldCanvas;
+    public GameObject StageCanvas;
+    public GameObject FirstSelect;
+
+    public void BackWorldSelecting()
+    {
+        WorldCanvas.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(FirstSelect);
+        StageCanvas.SetActive(false);
+    }
+
+    //private void Start()
+    //{
+    //    WorldCanvas = GameObject.Find("WorldSelectCanvas");
+    //}
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            BackWorldSelecting();
+        }
+    }
+
+}
--- Assets/Scenes/StageSelect/StageSelectManeger.cs
+++ Assets/Scenes/StageSelect/StageSelectManeger.cs
@@ -3,12 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using UnityEngine.EventSystems;
+
 public class StageSelectManeger : MonoBehaviour
 {
-    public void StageSelect(int stage)
+    //public GameObject WorldCanvas;
+    //public GameObject StageCanvas;
+    //public GameObject FirstSelect;
+
+    public void StageSelect(string stage)
     {
-        //番号順
-        //File...BuildSettings...
+        // ステージ名入力
         SceneManager.LoadScene(stage);
     }
+
+    //public void BackWorldSelecting()
+    //{
+    //    WorldCanvas.SetActive(true);
+    //    EventSystem.current.SetSelectedGameObject(FirstSelect);
+    //    StageCanvas.SetActive(false);
+    //}
+
+    //private void Start()
+    //{
+    //    WorldCanvas = GameObject.Find("WorldSelectCanvas");
+    //}
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyUp(KeyCode.X))
+    //    {
+    //        BackWorldSelecting();
+    //    }
+    //}
+
 }
