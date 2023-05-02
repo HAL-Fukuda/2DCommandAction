c7b9eb9e448d8cbc39d9513cc7824545df3dad38
rincha- <rincha1210@gmail.com>
2023-05-02 19:12:09 +0900

    ワールド４のギミック作成

---
+++ Assets/Scenes/World_4/Brittle.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brittle : MonoBehaviour
+{
+    public int maxHealth = 3; // 最大HP
+    public int currentHealth; // 現在のHP
+
+    private void Start()
+    {
+        currentHealth = maxHealth; // 初期化
+    }
+
+    void Update()
+    {
+        Debug.Log(currentHealth);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerManager playerManager = collision.GetComponent<PlayerManager>();
+            if (!playerManager.isGround)
+            {
+                currentHealth--; // HPを減らす
+
+                if (currentHealth <= 0)
+                {
+                    Destroy(gameObject); // HPが0以下になったらオブジェクトを破壊する
+                }
+            }
+        }
+
+        //if (collision.gameObject.CompareTag("EnemyAttack")) // EnemyAttackのプレハブに当たると
+        //{
+        //    currentHealth--; // HPを減らす
+
+        //    if (currentHealth <= 0)
+        //    {
+        //        Destroy(gameObject); // HPが0以下になったらオブジェクトを破壊する
+        //    }
+        //}
+    }
+}
