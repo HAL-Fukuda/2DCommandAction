d86fcde8d5f0326db383409fac6c9c27c6226890
松村　太雅 <tig13.0401@gmail.com>
2023-05-08 18:18:01 +0900

    足場に着地していると3秒ごとにダメージをくらう足場

---
+++ Assets/Scenes/World_5/DamageFloor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageFloor : MonoBehaviour
+{
+    private LifeManager lifeManager;
+    private BoxCollider2D boxCollider;
+    private float timer = 0f;
+    private bool isColliding = false;
+
+   // public AudioClip se;
+
+    void Start()
+    {
+        GameObject lifeManagerObject = GameObject.Find("Life");
+        lifeManager = lifeManagerObject.GetComponent<LifeManager>();
+        boxCollider = this.GetComponent<BoxCollider2D>();
+    }
+
+    public void ToggleCollider(bool enabled)
+    {
+        boxCollider.enabled = enabled;
+    }
+
+    private void Update()
+    {
+        if (isColliding)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 3f)
+            {
+                lifeManager.GetDamage(1);
+                timer = 0f;
+               // AudioSource.PlayClipAtPoint(se, transform.position);
+            }
+        }
+        else
+        {
+            timer = 0f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isColliding = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isColliding = false;
+        }
+    }
+}
