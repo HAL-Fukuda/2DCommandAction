d4a51b5b7b2e2aeb56a80aa8c0cc8cef3f152d18
kalta <kalta@MSI>
2023-05-19 18:47:19 +0900

    宇宙人パンチ(衝撃波付き)の実装

--- Assets/Scenes/InGame/Enemy/EnemyScript/EnemyAttack.cs
+++ Assets/Scenes/InGame/Enemy/EnemyScript/EnemyAttack.cs
@@ -118,7 +118,7 @@ public partial class EnemyAttack : MonoBehaviour //partial C++‚ÌƒNƒ‰ƒX
 
         if (Input.GetKeyUp(KeyCode.B))
         {
-            FlameSplash();
+            ClubBeating();
         }
     }
 
