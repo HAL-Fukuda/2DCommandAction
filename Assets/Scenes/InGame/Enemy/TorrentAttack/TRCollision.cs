db66118c65364dd8ba4966e7e65caa618cb82fb6
TiG401 <tig13.0401@gmail.com>
2023-05-21 18:56:44 +0900

    激流攻撃完成

---
+++ Assets/Scenes/InGame/Enemy/TorrentAttack/TRCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TRCollision : MonoBehaviour
+{
+    private LifeManager lifeManager;
+    public AudioClip BeatingSound;
+    //private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        // LifeManagerスクリプトがアタッチされているオブジェクトを取得する
+        GameObject lifeManagerObject = GameObject.Find("Life");
+
+        // LifeManagerコンポーネントを取得する
+        lifeManager = lifeManagerObject.GetComponent<LifeManager>();
+
+        //Componentを取得
+        //audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lifeManager.GetDamage(1);
+        }
+    }
+}
