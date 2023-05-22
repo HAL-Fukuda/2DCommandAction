a2e800df2fba261d24d8f0fcd11306162315b5f6
HAL_Fukuda <Ageh078@icloud.com>
2023-05-22 18:36:57 +0900

    マンドラゴラの叫び完成

--- Assets/Scenes/InGame/Enemy/ShoutAttack/Shout.cs
+++ Assets/Scenes/InGame/Enemy/ShoutAttack/Shout.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class Shout : MonoBehaviour
 {
-    private Transform target;
     float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        // プレイヤーをターゲットにする
-        GameObject player = GameObject.Find("Player");
-        target = player.transform;
-
-        // ターゲットの方向を向く
-        ForcusTarget();
-
         // カメラを揺らす
         CameraEffect cameraScript = GameObject.Find("Main Camera").GetComponent<CameraEffect>();
         cameraScript.ShakeCamera(0.2f, 1f);
@@ -31,18 +23,5 @@ public class Shout : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
-    }
-
-    // ターゲットの方向を向く
-    void ForcusTarget()
-    {
-        // ターゲットの位置から自分自身のオブジェクトの位置を引いて、方向ベクトルを求める。
-        Vector3 direction = target.position - this.transform.position;
-
-        // 方向ベクトルを使って、回転角度を求める。
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // 回転角度を自分自身のオブジェクトに適用する。
-        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
