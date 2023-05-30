0c0ff8be62a6ea6464164b6474c53ec136a411af
casestudy <rin20011210@outlook.jp>
2023-05-30 15:50:10 +0900

    こおるゆか左右移動で滑らないように もろいゆかの㏋回復するように

--- Assets/Scenes/World_4/Brittle.cs
+++ Assets/Scenes/World_4/Brittle.cs
@@ -6,8 +6,14 @@ public class Brittle : MonoBehaviour
 {
     public int maxHealth = 3; // 最大HP
     public int currentHealth; // 現在のHP
+    public float recoveryTime = 5f; // 回復までの時間（秒）
+    private bool isRecovering = false; // 回復中かどうかのフラグ
     Renderer objectRenderer;
-    public Material newMaterial;
+    public Material materialAtHealth1;
+    public Material materialAtHealth2;
+    public Material materialAtHealth3;
+
+    private float timer = 0.0f;
 
     private void Start()
     {
@@ -15,23 +21,60 @@ public class Brittle : MonoBehaviour
         objectRenderer = GetComponent<Renderer>();
     }
 
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if ((timer >= recoveryTime) && (currentHealth >= 1 && currentHealth <= 2))
+        {
+            // 体力回復
+            currentHealth++;
+            timer = 0; // タイマーリセット
+
+            if (currentHealth == 1)
+            {
+                objectRenderer.material = materialAtHealth1;
+            }
+            else if (currentHealth == 2)
+            {
+                objectRenderer.material = materialAtHealth2;
+            }
+            else if (currentHealth == 3)
+            {
+                objectRenderer.material = materialAtHealth3;
+            }
+
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             PlayerManager playerManager = collision.GetComponent<PlayerManager>();
-            if (!playerManager.isGround)
+            if (!playerManager.isGround && !isRecovering)
             {
                 currentHealth--; // HPを減らす
+                timer = 0.0f; // タイマーリセット
 
                 if (currentHealth <= 0)
                 {
                     Destroy(gameObject); // HPが0以下になったらオブジェクトを破壊する
                 }
-                else if (currentHealth == 1)
+
+                if (currentHealth == 1)
                 {
-                    objectRenderer.material = newMaterial;
+                    objectRenderer.material = materialAtHealth1;
                 }
+                else if (currentHealth == 2)
+                {
+                    objectRenderer.material = materialAtHealth2;
+                }
+                else if (currentHealth == 3)
+                {
+                    objectRenderer.material = materialAtHealth3;
+                }
+
             }
         }
     }
